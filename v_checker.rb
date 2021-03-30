1df9f5a082689f3faa7e82206412331410abb3f7
abdfnx <64256993+abdfnx@users.noreply.github.com>
2021-03-30 21:44:42 +0300

    change the message

--- v_checker.rb
+++ v_checker.rb
@@ -6,10 +6,12 @@ $c = `secman verx`
 
 def _n()
   ly = $l.cyan.bold
+  cy = $c.cyan.bold
+
   nr = "\nthere's a new release of secman is avalaible:".yellow
   up = "to upgrade run".yellow
   smu = "secman upg".cyan
-  puts new_r = "#{nr} #{ly}#{up} #{smu}"
+  puts new_r = "#{nr} #{cy} → #{ly}#{up} #{smu}"
 end
 
 def check()
